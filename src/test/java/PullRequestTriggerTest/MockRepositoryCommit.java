7657f70dc18b10fe1165f5e37cdb5bb665f427bb
Jason Weden <jweden@constantcontact.com>
2015-12-15 14:40:30 -0500

    more tests.

---
+++ src/test/java/PullRequestTriggerTest/MockRepositoryCommit.java
@@ -0,0 +1,9 @@
+package PullRequestTriggerTest;
+
+import org.eclipse.egit.github.core.RepositoryCommit;
+
+/**
+ *
+ */
+public class MockRepositoryCommit extends RepositoryCommit {
+}
--- src/test/java/PullRequestTriggerTest/RepositoryCommit.java
+++
@@ -1,7 +0,0 @@
-package PullRequestTriggerTest;
-
-/**
- *
- */
-public class RepositoryCommit {
-}
